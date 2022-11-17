dddceb9d92f9375247ccb9ad491e485d96de58d0
Lam Hoang <lhoang@infinera.com>
2022-11-16 16:02:51 -0800

    updated

--- network-config-intent.tf
+++ network-config-intent.tf
@@ -8,62 +8,63 @@ variable network {
                         modulation = optional(string)
                       })
       setup = map(object({ 
-                moduleconfig  = object( {fiberconnectionmode = optional(string)
-                                        tcmode = optional(bool)
-                                        configuredrole = optional(string)}),
-                moduleclients = list(string),
-                modulecarriers= list(object({lineptpid = string, carrierid = string}))
+                moduleconfig  = object( {fiberconnectionmode = optional(string),
+                                        tcmode = optional(bool),
+                                        configuredrole = optional(string),
+                                        trafficmode = optional(string)}),
+                moduleclients = list(object({clientid = string, portspeed = optional(string)})),
+                modulecarriers= list(object({lineptpid = string, carrierid = string, modulation = optional(string), clientportmode = optional(string), constellationfrequency = optional(number)}))
                 }))})
   description = "for each module, specify its config, it client port and line port "
   default = {
-      configs = { portspeed = "100G"
+      configs = { portspeed = ""
                   trafficmode = "L2Mode"
-                  modulation = "16QAM" 
+                  modulation = "" 
                 }
       setup = {
         xr-regA_H1-Hub = {
           moduleconfig = { configuredrole = "hub"}
-          moduleclients = ["1","2" ]
+          moduleclients = [{ clientid = "1"}, { clientid = "2"}]
           modulecarriers = [{ lineptpid = "1", carrierid = "1"}]
         }
         xr-regA_H1-L1 = {
           moduleconfig = { configuredrole = "leaf"}
-          moduleclients = ["1"]
+          moduleclients = [{ clientid = "1"}]
           modulecarriers = [{ lineptpid = "1", carrierid = "1"} ]
         }
         xr-regA_H1-L2 = {
           moduleconfig = { configuredrole = "leaf"}
-          moduleclients = ["1"]
+          moduleclients = [{ clientid = "1"}]
           modulecarriers = [{ lineptpid = "1", carrierid = "1"} ]
         }
         xr-regA_H1-L3 = {
           moduleconfig = { configuredrole = "leaf"}
-          moduleclients = ["1"]
+          moduleclients = [{ clientid = "1"}]
           modulecarriers = [{ lineptpid = "1", carrierid = "1"} ]
         }
         xr-regA_H1-L4 = {
           moduleconfig = { configuredrole = "leaf"}
-          moduleclients = ["1"]
+          moduleclients = [{ clientid = "1"}]
           modulecarriers = [{ lineptpid = "1", carrierid = "1"} ]
         }
         xr-regA_H2-L1 = {
           moduleconfig = { configuredrole = "leaf"}
-          moduleclients = ["1"]
+          moduleclients = [{ clientid = "1"}]
           modulecarriers = [{ lineptpid = "1", carrierid = "1"} ]
         }
         xr-regA_H2-L2 = {
           moduleconfig = { configuredrole = "leaf"}
-          moduleclients = ["1"]
+          moduleclients = [{ clientid = "1"}]
           modulecarriers = [{ lineptpid = "1", carrierid = "1"} ]
         }
         xr-regA_H2-L3 = {
           moduleconfig = { configuredrole = "leaf"}
-          moduleclients = ["1"]
+          moduleclients = [{ clientid = "1"}]
           modulecarriers = [{ lineptpid = "1", carrierid = "1"} ]
         }
         xr-regA_H2-L4 = {
           moduleconfig = { configuredrole = "leaf"}
-          moduleclients = ["1"]
+          moduleclients = [{ clientid = "1"}]
           modulecarriers = [{ lineptpid = "1", carrierid = "1"} ]
         }
       }
