766753844299e6a2900077d1883e52ebdc5cdae8
Lam Hoang <lhoang@infinera.com>
2022-11-17 08:15:39 -0800

    add multichecks

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ data "xrcm_onlinedevices" "onlinedevices" {
 // example chek for IPM control
 data "xrcm_check" "check_host_control" {
   condition = local.ipm_control
+  description = "IPM is in controlled"
   throw = "Host is the controller. Can't configure the network."
 }
 
@@ -20,14 +21,27 @@ data "xrcm_check" "check_host_control" {
 data "xrcm_check" "check_Hub_count" {
   depends_on        = [data.xrcm_onlinedevices.onlinehubdevices, data.xrcm_check.check_host_control]
   condition = length(data.xrcm_onlinedevices.onlinehubdevices.devices) >= 1 
+  description = " Number of Hub count which must be more than 1"
   throw = "Must have at least 2 Hubs"
 }
 
 // example check for config role
 data "xrcm_check" "check_leaf" {
   depends_on        = [data.xrcm_onlinedevices.onlinedevices, data.xrcm_check.check_host_control]
-  condition = length(data.xrcm_onlinedevices.onlinedevices.devices) >= 3 && data.xrcm_onlinedevices.onlinehubdevices.devices[0].configuredrole == "leaf" && data.xrcm_onlinedevices.onlinedevices.devices[1].configuredrole == "leaf" && data.xrcm_onlinedevices.onlinedevices.devices[2].configuredrole == "leaf"
+  condition = length(data.xrcm_onlinedevices.onlinedevices.devices) >= 3 && data.xrcm_onlinedevices.onlinedevices.devices[0].configuredrole == "leaf" && data.xrcm_onlinedevices.onlinedevices.devices[1].configuredrole == "leaf" && data.xrcm_onlinedevices.onlinedevices.devices[2].configuredrole == "leaf"
+  description = "All the listed devices's roles are leaf"
   throw = "All devices must have Hub role"
+}
+
+data "xrcm_checks" "check_host_control_and_leaf" {
+  depends_on        = [data.xrcm_onlinedevices.onlinedevices, data.xrcm_check.check_host_control, data.xrcm_check.check_leaf]
+  checks = [ { condition = length(data.xrcm_onlinedevices.onlinehubdevices.devices) >= 1 
+               description = " Number of Hub count which must be more than 1"
+               throw = "Must have at least 2 Hubs"}, 
+             { condition = length(data.xrcm_onlinedevices.onlinedevices.devices) >= 3 && data.xrcm_onlinedevices.onlinedevices.devices[0].configuredrole == "leaf" && data.xrcm_onlinedevices.onlinedevices.devices[1].configuredrole == "leaf" && data.xrcm_onlinedevices.onlinedevices.devices[2].configuredrole == "leaf"
+               description = "All the listed devices's roles are leaf"
+               throw = "All devices must have Hub role"}
+           ]
 }
 
 // Set up the Constellation Network
