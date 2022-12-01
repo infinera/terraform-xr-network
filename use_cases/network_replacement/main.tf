6cdd1e9e404ef0203ce7978b9e8ec67a4ebc7b5d
Lam Hoang <lhoang@infinera.com>
2022-12-01 13:44:16 -0800

    updated

--- use_cases/network_replacement/main.tf
+++ use_cases/network_replacement/main.tf
@@ -1,6 +1,7 @@
 
 module "network_device_replacement" {
   source = "git::https://github.com/infinera/terraform-infinera-xr-modules.git//workflows/network_device_replacement"
+  //source = "../../../terraform-infinera-xr-modules/workflows/network_device_replacement"
   
   assert = var.assert
   devices_file = var.devices_file
@@ -8,4 +9,8 @@ module "network_device_replacement" {
   leaf_bandwidth = var.leaf_bandwidth
   hub_bandwidth = var.hub_bandwidth
   client-2-dscg     = var.client-2-dscg
+}
+
+output "message" {
+  value = module.network_device_replacement.message
 }
--- use_cases/network_setup/main.tf
+++ use_cases/network_setup/main.tf
@@ -1,10 +1,18 @@
 
-module "setup-network_with_checks" {
-  source = "git::https://github.com/infinera/terraform-infinera-xr-modules.git//workflows/setup_network_with_checks"
-  //source = "../../../terraform-infinera-xr-modules/workflows/setup_network_with_checks"
+module "setup_network_with_checks" {
+  //source = "git::https://github.com/infinera/terraform-infinera-xr-modules.git//workflows/setup_network_with_checks"
+  source = "../../../terraform-infinera-xr-modules/workflows/setup_network_with_checks"
   
   asserts = var.asserts
   network = var.network
   leaf_bandwidth = var.leaf_bandwidth
   hub_bandwidth = var.hub_bandwidth
+}
+
+output "check_devices_version_message" {
+  value =  module.setup_network_with_checks.devices_version_check_message
+}
+
+output "host_attribute_mismatch_check_message" {
+  value =  module.setup_network_with_checks.host_attribute_mismatch_check_message
 }
